e9b14058a1a2525589796a872a451be5c2b747c3
m_u_say@hotmail.com <m_u_say@hotmail.com>
2022-03-22 09:22:10 +0900

    complete section-7-81

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,23 @@
-const child = document.querySelector(".child");
-const cb = function(entries, observer) {
-  entries.forEach(entry => {
-    if(entry.isIntersecting) {
-      // 対象がウィンドウ内に入った時
-      entry.target.classList.add("inview");
-      // ovserver.unobserve(entry.target); // entry.targetに監視対象のDOMが格納されている
-    } else {
-      // 対象がウィンドウ外に出た時
-      entry.target.classList.remove("inview");
-    }
-  });
-}
-const options = {
-  root: null, // 交差対象とする親の要素を指定
-  rootMargin: "-100px 0px 0px 0px",
-  threshold: [0, 0.5, 1]
-}
-const io = new IntersectionObserver(cb, options); // IntersectionObserverの第二引数にはオプションを指定できる
-io.observe(child);
+document.addEventListener('DOMContentLoaded', function () {
+  
+  const els = document.querySelectorAll('.animate-title');
+  const cb = function(entries, observer) {
+    entries.forEach(entry => {
+      if(entry.isIntersecting) {
+        const ta = new TextAnimation(entry.target);
+        ta.animate();
+        observer.unobserve(entry.target);
+      } else {
+      }
+    });
+  }
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0
+  };
+  const io = new IntersectionObserver(cb, options);
+  els.forEach(el => io.observe(el));
+});
+  
+  
