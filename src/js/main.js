1155cea7eb54ffdd6f7e227e7ce4bd54af1d9911
m_u_say@hotmail.com <m_u_say@hotmail.com>
2022-03-23 23:51:26 +0900

    complete section7-82

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
   
-  const els = document.querySelectorAll('.animate-title');
-  const cb = function(entries, observer) {
-    entries.forEach(entry => {
-      if(entry.isIntersecting) {
-        const ta = new TextAnimation(entry.target);
-        ta.animate();
-        observer.unobserve(entry.target);
-      } else {
-      }
-    });
+  const cb = function (el, isIntersecting) {
+    if (isIntersecting) {
+      const ta = new TextAnimation(el);
+      ta.animate();
+    }
   }
-  const options = {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0
-  };
-  const io = new IntersectionObserver(cb, options);
-  els.forEach(el => io.observe(el));
+
+  const so = new ScrollObserver(".animate-title", cb);
+  // const cb = function(entries, observer) {
+  //   entries.forEach(entry => {
+  //     if(entry.isIntersecting) {
+  //       const ta = new TextAnimation(entry.target);
+  //       ta.animate();
+  //       observer.unobserve(entry.target);
+  //     } else {
+  //     }
+  //   });
+  // }
+  // const options = {
+  //   root: null,
+  //   rootMargin: "0px",
+  //   threshold: 0
+  // };
 });
-  
-  
