1abd9519cd97a91a31d9eedcfdfe0807bfc35cd4
m_u_say@hotmail.com <m_u_say@hotmail.com>
2022-04-01 00:28:26 +0900

    complete section7-83

--- src/js/main.js
+++ src/js/main.js
@@ -2,25 +2,9 @@ document.addEventListener('DOMContentLoaded', function () {
   
   const cb = function (el, isIntersecting) {
     if (isIntersecting) {
-      const ta = new TextAnimation(el);
-      ta.animate();
+      el.classList.add("inview");
     }
   }
 
-  const so = new ScrollObserver(".animate-title", cb);
-  // const cb = function(entries, observer) {
-  //   entries.forEach(entry => {
-  //     if(entry.isIntersecting) {
-  //       const ta = new TextAnimation(entry.target);
-  //       ta.animate();
-  //       observer.unobserve(entry.target);
-  //     } else {
-  //     }
-  //   });
-  // }
-  // const options = {
-  //   root: null,
-  //   rootMargin: "0px",
-  //   threshold: 0
-  // };
+  const so = new ScrollObserver(".cover-slide", cb);
 });
